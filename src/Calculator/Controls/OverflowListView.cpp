619b7f06e2b0c1286e6755b02bb3f1e5a287596a
Pepe Rivera <joriv@microsoft.com>
2019-03-28 10:28:41 -0700

    scrolling

--- src/Calculator/Controls/OverflowListView.cpp
+++ src/Calculator/Controls/OverflowListView.cpp
@@ -9,6 +9,7 @@ using namespace CalculatorApp::Controls;
 
 using namespace Platform;
 using namespace std;
+using namespace Windows::Foundation;
 using namespace Windows::Devices::Input;
 using namespace Windows::UI::Xaml;
 using namespace Windows::UI::Xaml::Input;
@@ -25,14 +26,28 @@ void OverflowListView::OnApplyTemplate()
     m_scrollRight = safe_cast<Button^>(GetTemplateChild("ScrollRight"));
     m_border = safe_cast<Border^>(GetTemplateChild("Border"));
     m_content = safe_cast<ItemsPresenter^>(GetTemplateChild("Content"));
+    m_grid = safe_cast<Grid^>(GetTemplateChild("Grid"));
 
     m_scrollLeft->Click += ref new RoutedEventHandler(this, &OverflowListView::OnScrollClick);
     m_scrollRight->Click += ref new RoutedEventHandler(this, &OverflowListView::OnScrollClick);
 
-    m_border->PointerEntered += ref new PointerEventHandler(this, &OverflowListView::OnPointerEntered);
-    m_border->PointerExited += ref new PointerEventHandler(this, &OverflowListView::OnPointerExited);
+    m_scrollViewer->ViewChanged += ref new EventHandler<ScrollViewerViewChangedEventArgs^>(this, &OverflowListView::ScrollViewChanged);
+
+    this->PointerEntered += ref new PointerEventHandler(this, &OverflowListView::OnPointerEntered);
+    this->PointerExited += ref new PointerEventHandler(this, &OverflowListView::OnPointerExited);
+
+    m_scrollLeft->PointerExited += ref new PointerEventHandler(this, &OverflowListView::OnButtonPointerExited);
+    m_scrollRight->PointerExited += ref new PointerEventHandler(this, &OverflowListView::OnButtonPointerExited);
 
     ListView::OnApplyTemplate();
+}
+
+void OverflowListView::ScrollViewChanged(_In_ Object^, _In_ ScrollViewerViewChangedEventArgs^ e)
+{
+    if (m_isPointerEntered && !e->IsIntermediate)
+    {
+        UpdateScrollButtons();
+    }
 }
 
 void OverflowListView::OnScrollClick(_In_ Object^ sender, _In_ RoutedEventArgs^)
@@ -48,36 +63,44 @@ void OverflowListView::OnScrollClick(_In_ Object^ sender, _In_ RoutedEventArgs^)
     }
 }
 
+
+void OverflowListView::OnButtonPointerExited(_In_ Object^ sender, _In_ PointerRoutedEventArgs^ e)
+{
+    auto button = static_cast<Button^>(sender);
+
+    // Do not bubble up the pointer exit event to the control if the button being exited was not visible
+    if (button->Visibility == ::Visibility::Collapsed)
+    {
+        e->Handled = true;
+    }
+}
+
 void OverflowListView::OnPointerEntered(_In_ Object^ sender, _In_ PointerRoutedEventArgs^ e)
 {
     if (e->Pointer->PointerDeviceType == PointerDeviceType::Mouse)
     {
         UpdateScrollButtons();
+        m_isPointerEntered = true;
     }
 }
 
-void OverflowListView::OnPointerExited(_In_ Object^ sender, _In_ PointerRoutedEventArgs^)
+void OverflowListView::OnPointerExited(_In_ Object^ sender, _In_ PointerRoutedEventArgs^ e)
 {
     m_scrollLeft->Visibility = ::Visibility::Collapsed;
     m_scrollRight->Visibility = ::Visibility::Collapsed;
+    m_isPointerEntered = false;
 }
 
 void OverflowListView::ScrollLeft()
 {
         double offset = m_scrollViewer->HorizontalOffset - (scrollRatio * m_scrollViewer->ViewportWidth);
-        m_scrollViewer->UpdateLayout();
         m_scrollViewer->ChangeView(offset, nullptr, nullptr);
-        m_scrollViewer->UpdateLayout();
-        UpdateScrollButtons();
 }
 
 void OverflowListView::ScrollRight()
 {
         double offset = m_scrollViewer->HorizontalOffset + (scrollRatio * m_scrollViewer->ViewportWidth);
-        m_scrollViewer->UpdateLayout();
         m_scrollViewer->ChangeView(offset, nullptr, nullptr);
-        m_scrollViewer->UpdateLayout();
-        UpdateScrollButtons();
 }
 
 
--- src/Calculator/Controls/OverflowListView.h
+++ src/Calculator/Controls/OverflowListView.h
@@ -21,6 +21,8 @@ namespace CalculatorApp
             void OnScrollClick(_In_ Platform::Object^ sender, _In_ Windows::UI::Xaml::RoutedEventArgs^ e);
             void OnPointerEntered(_In_ Platform::Object^ sender, _In_ Windows::UI::Xaml::Input::PointerRoutedEventArgs^ e);
             void OnPointerExited(_In_ Platform::Object^ sender, _In_ Windows::UI::Xaml::Input::PointerRoutedEventArgs^ e);
+            void OnButtonPointerExited(_In_ Platform::Object^ sender, _In_ Windows::UI::Xaml::Input::PointerRoutedEventArgs^ e);
+            void ScrollViewChanged(_In_ Platform::Object^ sender, _In_ Windows::UI::Xaml::Controls::ScrollViewerViewChangedEventArgs^ args);
             void ShowHideScrollButtons(Windows::UI::Xaml::Visibility vLeft, Windows::UI::Xaml::Visibility vRight);
             void OnTokensUpdatedPropertyChanged(bool oldValue, bool newValue);
             void UpdateScrollButtons();
@@ -31,14 +33,18 @@ namespace CalculatorApp
             void ScrollLeft();
             void ScrollRight();
 
-            double scrollRatio = 1.0;
+            double scrollRatio = 0.7;
             bool m_scrollingLeft;
             bool m_scrollingRight;
+
+            bool m_isPointerEntered;
+
             Windows::UI::Xaml::Controls::ItemsPresenter^ m_content;
             Windows::UI::Xaml::Controls::Border^ m_border;
             Windows::UI::Xaml::Controls::ScrollViewer^ m_scrollViewer;
             Windows::UI::Xaml::Controls::Button^ m_scrollLeft;
             Windows::UI::Xaml::Controls::Button^ m_scrollRight;
+            Windows::UI::Xaml::Controls::Grid^ m_grid;
 
             Windows::Foundation::EventRegistrationToken m_scrollLeftClickEventToken;
             Windows::Foundation::EventRegistrationToken m_scrollRightClickEventToken;
